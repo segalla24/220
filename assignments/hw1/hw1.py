e6d61c9aad42c45e25faf075e89c1787bd9773b0
logansegal <segalla@g.cofc.edu>
2022-01-21 09:15:49 -0500

    completed homework 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,53 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Logan Segal
+segal-homework 1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: In this homework I was able to get more comfortable with python and using simple functions. I was able to create
+    functions that can help me do simple problems by collecting data
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("Volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    total_shots = eval(input("Enter the players total shots: "))
+    shots_made = eval(input("Enter how many shots the player made: "))
+    shooting_ratio = shots_made / total_shots
+    shooting_percentage = shooting_ratio * 100
+    print("Shooting Percentage= ", shooting_percentage)
 
 
 def coffee():
-    pass
+    coffee_amount = eval(input("How many pounds of coffee would you like?"))
+    coffee_cost = 10.50
+    shipping_cost = 0.86
+    fixed_cost = 1.50
+    total = coffee_amount * coffee_cost + coffee_amount * shipping_cost + fixed_cost
+    print("Your total is:", total)
 
 
 def kilometers_to_miles():
-    pass
+    kilometer = eval(input("How many kilometers did you travel?"))
+    mile = kilometer * 0.621371
+    round_mile = round(mile,1)
+    print("That's",round_mile,"miles!")
+
 
 
 if __name__ == '__main__':
