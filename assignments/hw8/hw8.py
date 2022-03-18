935966612dd7af15330ed0f2d9e361b9011ce16f
logansegal <segalla@g.cofc.edu>
2022-03-18 16:49:44 -0400

    completed homework 8

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -1,42 +1,64 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Logan Segal
+Homework 8.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: In this homework we practiced working with parameters and conditionals,
+    specifically conditional control structures. We combined this knowledge with
+    previous knowledge and topics we have learned in the past to complete this
+    homework.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
+from graphics import GraphWin, Circle, Text, Point
 
 
 def add_ten(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = nums[i] + 10
 
 
 def square_each(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = nums[i] ** 2
 
 
 def sum_list(nums):
-    pass
+    num_sum = sum(nums)
+    return num_sum
 
 
 def to_numbers(nums):
-    pass
+    for i in range(len(nums)):
+        nums[i] = eval(nums[i])
 
 
 def sum_of_squares(nums):
-    pass
+    my_list = []
+    for i in range(len(nums)):
+        num_split = nums[i].split(", ")
+        to_numbers(num_split)
+        square_each(num_split)
+        value = sum_list(num_split)
+        my_list.append(value)
+    return my_list
 
 
 def starter(weight, wins):
-    pass
+    if 150 <= weight < 160 and wins >= 5:
+        return True
+    elif weight > 199 or wins > 20:
+        return True
+    else:
+        return False
 
 
 def leap_year(year):
-    pass
+    if year % 400 == 0 or year % 100 and year % 4 == 0:
+        return True
+    else:
+        return False
 
 
 def circle_overlap():
@@ -55,12 +77,52 @@ def circle_overlap():
     circle_one.setFill("light blue")
     circle_one.draw(win)
 
+    center2 = win.getMouse()
+    circumference2 = win.getMouse()
+    radius2 = math.sqrt(
+        (center2.getX() - circumference2.getX()) ** 2 + (center2.getY() - circumference2.getY()) ** 2)
+    circle_two = Circle(center2, radius2)
+    circle_two.setFill("light green")
+    circle_two.draw(win)
+
+    true_msg = Text(Point(5, 4), "The circles overlap.")
+    false_msg = Text(Point(5, 4), "The circles do not overlap.")
+    close_msg = Text(Point(5, 3), "Click again to close.")
+
+    if did_overlap(circle_one, circle_two):
+        true_msg.draw(win)
+        close_msg.draw(win)
+    else:
+        false_msg.draw(win)
+        close_msg.draw(win)
+
     win.getMouse()
 
+circle_overlap()
 
 def did_overlap(circle_one, circle_two):
-    pass
+    center_1 = circle_one.getCenter()
+    center_2 = circle_two.getCenter()
+    c1x = center_1.getX()
+    c1y = center_1.getY()
+    c2x = center_2.getX()
+    c2y = center_2.getY()
 
+    x_equation = (c2x - c1x) ** 2
+    y_equation = (c2y - c1y) ** 2
+    distance = math.sqrt(x_equation + y_equation)
+
+    radius_1 = circle_one.getRadius()
+    radius_2 = circle_two.getRadius()
+    radii_sum = radius_2 + radius_1
+
+    if distance <= radii_sum:
+        print("The circles overlap.")
+        return True
+    else:
+        print("The circles do not overlap.")
+        return False
+did_overlap()
 
 if __name__ == '__main__':
     pass
